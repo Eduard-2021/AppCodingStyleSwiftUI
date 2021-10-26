a3d7e47c379a2424319047b3d796c958486a9b66
Eduard-2021 <sergsim1978@gmail.com>
2021-10-26 20:16:34 +0300

    Initial Commit

---
+++ AppCodingStyleSwiftUI/AppCodingStyleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCodingStyleSwiftUIApp.swift
+//  AppCodingStyleSwiftUI
+//
+//  Created by Eduard on 26.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct AppCodingStyleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
